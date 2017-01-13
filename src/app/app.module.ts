536e46f874fb8e0e5c723c8fbe2eaeec4f59c284
Scott Cooper <scttcper@gmail.com>
2017-01-12 22:08:17 -0800

    use hash

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, { useHash: true }),
     AdsenseModule.forRoot({
       adClient: 'ca-pub-7640562161899788',
       adSlot: 2930227358,
