cc591677d1dea1526e54c6134c8abaccc39f9d95
Scott Cooper <scttcper@gmail.com>
2017-02-25 10:11:48 -0800

    upgrade to rc1 and drop animations in demo

--- src/app/page.component.ts
+++ src/app/page.component.ts
@@ -1,12 +1,10 @@
 import { Component, HostBinding } from '@angular/core';
-import { routerTransition } from './router.animations';
 
 /**
  * Uses global values
  */
 @Component({
   selector: 'ng2-page-1',
-  animations: [routerTransition()],
   template: `
   <p class="text-center">Current View: {{title}}</p>
   <ng2-adsense></ng2-adsense>
@@ -14,14 +12,12 @@ import { routerTransition } from './router.animations';
   `,
 })
 export class PageComponent {
-  @HostBinding('@routerTransition') transition = '';
   title = 'Page 1';
 }
 
 
 @Component({
   selector: 'ng2-page-2',
-  animations: [routerTransition()],
   template: `
   <p class="text-center">Current View: {{title}}</p>
   <ng2-adsense [adClient]="'ca-pub-7640562161899788'" [adSlot]="2930227358"></ng2-adsense>
@@ -29,6 +25,5 @@ export class PageComponent {
   `,
 })
 export class OtherPageComponent extends PageComponent {
-  @HostBinding('@routerTransition') transition = '';
   title = 'Page 2';
 }
