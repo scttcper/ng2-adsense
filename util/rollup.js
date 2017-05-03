4c7243842b3bf37daa6b54bd0da3695686ebd3b0
Scott Cooper <scttcper@gmail.com>
2017-05-02 21:26:04 -0700

    upgrade packages and add source maps to umd

--- util/rollup.js
+++ util/rollup.js
@@ -1,9 +1,20 @@
+const sourcemaps = require('rollup-plugin-sourcemaps');
+
 export default {
   entry: './deploy/ng2-adsense.js',
   dest: './deploy/ng2-adsense.umd.js',
   format: 'umd',
   moduleName: 'ngadsense',
+  sourceMap: true,
   globals: {
     '@angular/core': 'ng.core',
-  }
+    '@angular/common': 'ng.common',
+  },
+  external: [
+    '@angular/core',
+    '@angular/common',
+  ],
+  plugins: [
+    sourcemaps()
+  ]
 };
