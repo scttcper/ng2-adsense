16bf0d3236a28787e449b7b6cbe9d288bebc3e86
Scott Cooper <scttcper@gmail.com>
2018-02-28 23:04:30 -0800

    switch to build file, fix active button in demo

---
+++ build.ts
@@ -0,0 +1,25 @@
+import { copySync } from 'fs-extra';
+import { build } from 'ng-packagr';
+import { join } from 'path';
+import * as rimraf from 'rimraf';
+
+async function main() {
+  // cleanup dist
+  rimraf.sync(join(process.cwd(), '/dist'));
+
+  // build package
+  await build({
+    project: join(process.cwd(), 'src/lib/package.json'),
+  });
+
+  // copy everything else
+  copySync('README.md', join(process.cwd(), 'dist/README.md'));
+  copySync('LICENSE', join(process.cwd(), 'dist/LICENSE'));
+}
+
+main()
+  .then(() => console.log('success'))
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
--- prepublish.js
+++
@@ -1,4 +0,0 @@
-const cpx = require('cpx');
-
-cpx.copy('README.md', 'dist');
-cpx.copy('LICENSE', 'dist');
