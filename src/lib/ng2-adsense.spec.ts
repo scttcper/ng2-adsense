f05551efb7ea4231b95a472a3038d06af398bbf6
Scott Cooper <scttcper@gmail.com>
2018-05-08 22:10:55 -0700

    chore: formatting and less renovate

--- src/lib/ng2-adsense.spec.ts
+++ src/lib/ng2-adsense.spec.ts
@@ -9,28 +9,23 @@ describe('AdsenseComponent', () => {
     adSlot: 2930227358,
     layout: 'z1',
   };
-  beforeEach(
-    async(() => {
-      TestBed.configureTestingModule({
-        imports: [AdsenseModule.forRoot(options)],
-      }).compileComponents();
-    })
-  );
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [AdsenseModule.forRoot(options)],
+    }).compileComponents();
+  }));
 
-  it(
-    'should render ng adsense',
-    async(() => {
-      const fixture = TestBed.createComponent(AdsenseComponent);
-      fixture.detectChanges();
-      const compiled = fixture.debugElement.nativeElement;
-      const ad = compiled.querySelector('ins');
-      expect(ad.className).toEqual('adsbygoogle');
-      expect(ad.getAttribute('data-ad-slot')).toEqual(String(options.adSlot));
-      expect(ad.getAttribute('data-ad-client')).toEqual(options.adClient);
-      expect(ad.getAttribute('data-layout')).toEqual('z1');
-      expect(ad.getAttribute('data-layout-key')).toEqual(null);
-      expect(ad.getAttribute('height')).toEqual(null);
-      expect(ad.getAttribute('width')).toEqual(null);
-    })
-  );
+  it('should render ng adsense', async(() => {
+    const fixture = TestBed.createComponent(AdsenseComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    const ad = compiled.querySelector('ins');
+    expect(ad.className).toEqual('adsbygoogle');
+    expect(ad.getAttribute('data-ad-slot')).toEqual(String(options.adSlot));
+    expect(ad.getAttribute('data-ad-client')).toEqual(options.adClient);
+    expect(ad.getAttribute('data-layout')).toEqual('z1');
+    expect(ad.getAttribute('data-layout-key')).toEqual(null);
+    expect(ad.getAttribute('height')).toEqual(null);
+    expect(ad.getAttribute('width')).toEqual(null);
+  }));
 });
