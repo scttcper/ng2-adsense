410a407e08ad6c89025ddd80773627e2870cae3d
Scott Cooper <scttcper@gmail.com>
2020-04-25 16:32:57 -0700

    chore: use node fs

--- build.ts
+++ build.ts
@@ -1,4 +1,4 @@
-import { copySync } from 'fs-extra';
+import { copyFileSync } from 'fs';
 import { build } from 'ng-packagr';
 import { join } from 'path';
 
@@ -9,8 +9,8 @@ async function main() {
   });
 
   // copy everything else
-  copySync('README.md', join(process.cwd(), 'dist/README.md'));
-  copySync('LICENSE', join(process.cwd(), 'dist/LICENSE'));
+  copyFileSync('README.md', join(process.cwd(), 'dist/README.md'));
+  copyFileSync('LICENSE', join(process.cwd(), 'dist/LICENSE'));
 }
 
 main()
