ddf3e046b675c3f76b5952771ad9658b3bd609a6
Scott Cooper <scttcper@gmail.com>
2023-10-01 20:49:35 -0700

    chore: upgrade dep

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     customLaunchers: {
-      ChromeHeadlessCustom: {
+      ChromeCI: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox', '--disable-gpu'],
       },
